d5e0ac0b45ff27f3c8dbe47a7df9847c451fc513
Vasil Bit <vasil.bituni@gmail.com>
2021-04-16 13:42:09 +0200

    Add first functional logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import renderInfo from './render';
+
 const inputEl = document.getElementById('city-name');
 const searchBtn = document.getElementById('search-btn');
 
@@ -7,7 +9,7 @@ function getWeather(e) {
 
   if (name) {
     fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${name}&appid=06390dd87d5264ce0e550a12e2f79b20`,
+      `http://api.openweathermap.org/data/2.5/weather?q=${name}&units=metric&appid=06390dd87d5264ce0e550a12e2f79b20`,
       {
         mode: 'cors',
       }
@@ -31,20 +33,26 @@ function filterWeatherData(data) {
   console.log({
     name: data.name,
     description: data.weather[0].description,
+    icon: data.weather[0].icon,
     temp: data.main.temp,
     feels: data.main.feels_like,
     humidity: data.main.humidity,
     wind: data.wind.speed,
+    pressure: data.main.pressure,
   });
 
-  return {
+  const weatherData = {
     name: data.name,
     description: data.weather[0].description,
+    icon: data.weather[0].icon,
     temp: data.main.temp,
     feels: data.main.feels_like,
     humidity: data.main.humidity,
     wind: data.wind.speed,
+    pressure: data.main.pressure,
   };
+
+  renderInfo(weatherData);
 }
 
 searchBtn.addEventListener('click', (e) => {
