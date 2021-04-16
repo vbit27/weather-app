540ffc057d39f070ccfb689aef95ef2f62a88235
Vasil Bit <vasil.bituni@gmail.com>
2021-04-16 10:14:23 +0200

    Add catch fetch error hanlding

--- src/index.js
+++ src/index.js
@@ -5,35 +5,32 @@ function getWeather(e) {
   e.preventDefault();
   const name = inputEl.value;
 
-  const info = fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${name}&appid=06390dd87d5264ce0e550a12e2f79b20`,
-    {
-      mode: 'cors',
-    }
-  )
-    .then(handleErrors)
-    .then(() => console.log('pk'))
-    .catch((error) => console.log(error));
-
-  if (info.cod === '404') {
-    return Promise.reject('er');
-  }
-
-  return {
-    name,
-    description: info.weather[0].description,
-    temp: info.main.temp,
-    feels: info.main.feels_like,
-    humidity: info.main.humidity,
-    wind: info.wind.speed,
-  };
-}
-
-function handleErrors(response) {
-  if (!response.ok) {
-    throw Error(response.statusText);
-  }
-  return response;
+  if (name) {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${name}&appid=06390dd87d5264ce0e550a12e2f79b20`,
+      {
+        mode: 'cors',
+      }
+    )
+      .then((resp) => resp.json())
+      .then((resp) => {
+        if (resp.cod && resp.cod === '404') {
+          throw Error(resp.message);
+        }
+        return resp;
+      })
+      .then((resp) => ({
+        name,
+        description: resp.weather[0].description,
+        temp: resp.main.temp,
+        feels: resp.main.feels_like,
+        humidity: resp.main.humidity,
+        wind: resp.wind.speed,
+      }))
+      .catch((err) => {
+        console.log(err);
+      });
+  } else alert('Put a valid city name');
 }
 
 searchBtn.addEventListener('click', (e) => {
